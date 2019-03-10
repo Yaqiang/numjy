3122698f2b98a26c336066a02449ba3e50e498c5
Yaqiang <yaqiang.wang@gmail.com>
2019-03-11 00:06:52 +0800

    first commit

---
+++ numjy/__init__.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+jarpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'javalib/NumJy-0.1.0-SNAPSHOT.jar')
+if not jarpath in sys.path:
+    sys.path.append(jarpath)
+
+from .core import *
